a844eb55fddb79a60af83c80d3271865e5305007
Indrarjun <billasaketh@gmail.com>
2023-01-22 19:21:28 +0530

    Created A Table of Todo In PostgreSQL

--- TodoList/settings.py
+++ TodoList/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Todos'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'TodoList.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'TodoDB',
+        'USER':'postgres',
+        'PASSWORD':'ammanana204',
+        'HOST':'localhost'
     }
 }
 
